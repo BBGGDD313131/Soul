e52091b3b4a8a455e26a7fd079ae41ced59d230e
BBGGDD313131 <pand.apapa3131@gmail.com>
2024-10-18 21:22:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8002780141:AAFkEmCbvIAwQCmfSnbowM52nj_8SR3ZGGM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("WHITE_X_OWNER")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1787873408")
+
+
+
+
+
+
+
+  
